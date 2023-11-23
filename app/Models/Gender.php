95d4dd660c1c99ee51a8f49b4d77afa63cbc0eb0
Robert Santos <robertsantosti@outlook.com>
2023-11-23 20:23:26 -0300

    Criando relação inversa entre Banda e Genero

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Gender extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+
+    /**
+     * Get the bands that owns the Gender
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function bands(): BelongsTo
+    {
+        return $this->belongsTo(Band::class);
+    }
 }
